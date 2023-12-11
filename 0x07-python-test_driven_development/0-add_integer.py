f75277288c7b49d142f424c0d53cca05609df342
amr-yassir <amryassir66@gmail.com>
2023-12-11 18:05:20 +0000

    update

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module to add two numbers"""
+
+
+def add_integer(a, b=98):
+    """adds two numbers"""
+
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
