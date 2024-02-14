14b52a3d711bb4ee6c1bc95e8f68287733b7f6bc
amr-yassir <amryassir66@gmail.com>
2024-02-14 13:30:07 +0000

    Update

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         rec = ""
-        rec += "\n".join(str(self.print_symbol) * self.__width
+        rec += "\n".join(str(self.print_symbol)*self.__width
                 for i in range(self.__height))
         return rec
 
