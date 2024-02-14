38fb8894517414ea661003de8c3c534d5dc7f051
amr-yassir <amryassir66@gmail.com>
2024-02-14 12:17:17 +0000

    Update

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -35,7 +35,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        slef.__height = value
+        self.__height = value
 
     def area(self):
         """returns the area of the rectangle"""
