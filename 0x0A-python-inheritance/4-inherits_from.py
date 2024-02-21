6bbd99db0c988e1aebd081c3edc6d018f2933a08
amr-yassir <amryassir66@gmail.com>
2024-02-21 20:48:59 +0000

    Update

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+"""Module for only sub class of"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class that inherited"""
+    return isinstance(obj, a_class)
