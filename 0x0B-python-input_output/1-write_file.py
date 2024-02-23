b60ff2350c94559d1f9a50e09011f34e95085e07
amr-yassir <amryassir66@gmail.com>
2024-02-23 12:46:55 +0000

    Update

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module'''
+
+
+def write_file(filename="", text=""):
+    '''writes a string to a text file (UTF8) and returns the number of characters'''
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
+        for char in text:
+            char += 1
+        return char
