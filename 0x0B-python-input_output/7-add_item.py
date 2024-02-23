d502c4afad17791844c5fdd250ccca40607d14ca
amr-yassir <amryassir66@gmail.com>
2024-02-23 16:55:52 +0000

    Update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,11 +7,8 @@ from 6-load_from_json_file import load_from_json_file
 
 try:
     data = load_from_json_file("add_item.json")
-    if not isinstance(data, list):
-        raise TypeError("Data loaded from JSON is not a list")
 except FileNotFoundError:
     data = []
 
 data.extend(sys.argv[1:])
 save_to_json_file(data, "add_item.json")
-
