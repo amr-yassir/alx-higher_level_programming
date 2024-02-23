7532fc405fe427e9b1d84c99ba4ff681369dbc21
amr-yassir <amryassir66@gmail.com>
2024-02-23 12:49:09 +0000

    Update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,4 @@ def write_file(filename="", text=""):
     '''writes a string to a text file (UTF8)
     returns the number of characters'''
     with open(filename, "w", encoding='utf-8') as f:
-        f.write(text)
-        for char in text:
-            char += 1
-        return char
+        return f.write(text)
