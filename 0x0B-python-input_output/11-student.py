ce695d45bd5d84a364497c8124c46b9723c35728
amr-yassir <amryassir66@gmail.com>
2024-02-23 16:47:37 +0000

    Update

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,7 +6,7 @@ class Student:
     '''defines a student'''
     def __init__(self, first_name, last_name, age):
         '''init'''
-        self.fisrt_name = first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
