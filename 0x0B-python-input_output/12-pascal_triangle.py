e598bde4c0a49030c77db8220ff98897be66d1d7
amr-yassir <amryassir66@gmail.com>
2024-02-27 12:06:14 +0000

    Update

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''technical interview module'''
+
+
+def pascal_triangle(n):
+    '''returns list of lists of integers representing Pascal’s triangle'''
+    if n <= 0:
+        return []
+
+    list_n = [[1]]
+    while len(list_n) != n:
+        list_t = list_n[-1]
+        temp = [1]
+        for i in range(len(list_t) - 1):
+            temp.append(list_t[i] + list_t[i + 1])
+        temp.append(1)
+        list_t.append(temp)
+    return list_t
